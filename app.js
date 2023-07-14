5c1973577caf3a9c249003422c39dd00642d3e77
P1kumarMhaske <104865937+Pavankumar-Mhaske@users.noreply.github.com>
2023-07-14 12:55:29 +0000

    added morgon and express urls

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import  express from "express"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+import cors from "cors"
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+app.use(cookieParser());
+
+// morgan logger
+app.use(morgan('tiny'));
+ 
+
 
 export default app ;
 
