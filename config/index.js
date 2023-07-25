cdd304e54d6b6921522520905571bb88baff90b6
P1kumarMhaske <104865937+Pavankumar-Mhaske@users.noreply.github.com>
2023-07-25 10:27:12 +0000

    created a mail utility

--- config/index.js
+++ config/index.js
@@ -11,6 +11,7 @@ const config = {
     SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT, 
     SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
     SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
+    SMTP_MAIL_EMAIL:process.env.SMTP_MAIL_EMAIL,
 
 }
 
---
+++ utils/mailHelper.js
@@ -0,0 +1,16 @@
+import transporter from "../config/transporter.config.js";
+import config from "../config/index.js";
+
+const mailHelper = async (options) => {
+    const message = {
+        from: SMTP_MAIL_EMAIL, // sender address
+        to: options.email , // list of receivers
+        subject: options.subject , // Subject line
+        text: options.text , // plain text body
+        // html: "<b>Hello world?</b>", // html body
+      }
+
+      await transporter.sendMail(message);
+}
+
+export default mailHelper;
