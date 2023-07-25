b39adc72dc189b33bd319046de906ee4afc41b09
P1kumarMhaske <104865937+Pavankumar-Mhaske@users.noreply.github.com>
2023-07-25 10:57:10 +0000

    changes made in forgot password

--- Controllers/auth.controllers.js
+++ Controllers/auth.controllers.js
@@ -1,7 +1,7 @@
 import User from '../models/user.schema.js';    // Importing User Schema
 import asyncHandler from '../services/asyncHandler';   // Importing express-async-handler
 import CustomError from '../utils/customError';     // Importing Custom Error Handler
-
+import mailHelper from '../utils/mailHelper';       // Importing Mail Helper
 
 export const cookieOptions = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // 3 days
@@ -123,6 +123,54 @@ export const logout = asyncHandler(async (_req, res) => {
         success: true,
         message: 'Logged out successfully',
     });
+});
+
+
+/******************************************************
+ * 
+ * @FORGOT_PASSWORD
+ * @route http://localhost:5000/api/v1/auth/password/forgot
+ * @description  user will submit email and will get a generate a token to
+ * @parameters  email
+ * @returns success message - email sent
+ ******************************************************/
+
+export const forgotPassword = asyncHandler(async (req, res) => {
+    const {email} = body.email;
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new CustomError('User not found', 404);
+    }
+
+    const resetToken = user.generateForgotPasswordToken();
+    await user.save({validateBeforeSave: false});
+
+    // creating an url for the user to reset the password
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/password/reset/${resetToken}`;
+
+    const text = `Your password reset token is as follows: \n\n ${resetUrl}`;
+    try {
+        await mailHelper({
+            email: user.email,
+            subject: 'Password reset token',
+            text: text,
+        }) // pass the options
+        res.status(200).json({
+            success: true,
+            message: `Email sent ${user.email}}`,
+        });
+    } catch (error) {
+
+        // role back - clear fields and save
+        user.forgotPasswordToken = undefined;
+        user.forgotPasswordExpiry = undefined;
+
+        user.save({validateBeforeSave: false});
+        throw new CustomError( error.message ||  'Email could not be sent', 500);
+    }
+
 });
 
 
